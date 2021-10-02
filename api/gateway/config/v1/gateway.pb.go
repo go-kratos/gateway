6ddee895734bed93cfad1f8b266491ed257c9975
chenzhihui <zhihui_chen@foxmail.com>
2021-10-02 17:00:47 +0800

    update api

--- api/gateway/config/v1/gateway.pb.go
+++ api/gateway/config/v1/gateway.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.27.1
 // 	protoc        v3.17.3
-// source: gateway/config/v1/gateway.proto
+// source: gateway.proto
 
 package api
 
@@ -32,7 +32,7 @@ type Gateway struct {
 func (x *Gateway) Reset() {
 	*x = Gateway{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_gateway_config_v1_gateway_proto_msgTypes[0]
+		mi := &file_gateway_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -45,7 +45,7 @@ func (x *Gateway) String() string {
 func (*Gateway) ProtoMessage() {}
 
 func (x *Gateway) ProtoReflect() protoreflect.Message {
-	mi := &file_gateway_config_v1_gateway_proto_msgTypes[0]
+	mi := &file_gateway_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -58,7 +58,7 @@ func (x *Gateway) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Gateway.ProtoReflect.Descriptor instead.
 func (*Gateway) Descriptor() ([]byte, []int) {
-	return file_gateway_config_v1_gateway_proto_rawDescGZIP(), []int{0}
+	return file_gateway_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Gateway) GetName() string {
@@ -86,7 +86,7 @@ type Listener struct {
 func (x *Listener) Reset() {
 	*x = Listener{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_gateway_config_v1_gateway_proto_msgTypes[1]
+		mi := &file_gateway_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -99,7 +99,7 @@ func (x *Listener) String() string {
 func (*Listener) ProtoMessage() {}
 
 func (x *Listener) ProtoReflect() protoreflect.Message {
-	mi := &file_gateway_config_v1_gateway_proto_msgTypes[1]
+	mi := &file_gateway_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -112,7 +112,7 @@ func (x *Listener) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Listener.ProtoReflect.Descriptor instead.
 func (*Listener) Descriptor() ([]byte, []int) {
-	return file_gateway_config_v1_gateway_proto_rawDescGZIP(), []int{1}
+	return file_gateway_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Listener) GetAddress() string {
@@ -122,43 +122,42 @@ func (x *Listener) GetAddress() string {
 	return ""
 }
 
-var File_gateway_config_v1_gateway_proto protoreflect.FileDescriptor
+var File_gateway_proto protoreflect.FileDescriptor
 
-var file_gateway_config_v1_gateway_proto_rawDesc = []byte{
-	0x0a, 0x1f, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67,
-	0x2f, 0x76, 0x31, 0x2f, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x12, 0x11, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x63, 0x6f, 0x6e, 0x66, 0x69,
-	0x67, 0x2e, 0x76, 0x31, 0x22, 0x58, 0x0a, 0x07, 0x47, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x12,
-	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
-	0x61, 0x6d, 0x65, 0x12, 0x39, 0x0a, 0x09, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x73,
-	0x18, 0x02, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x1b, 0x2e, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79,
-	0x2e, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x76, 0x31, 0x2e, 0x4c, 0x69, 0x73, 0x74, 0x65,
-	0x6e, 0x65, 0x72, 0x52, 0x09, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x73, 0x22, 0x24,
-	0x0a, 0x08, 0x4c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64,
-	0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x64, 0x64,
-	0x72, 0x65, 0x73, 0x73, 0x42, 0x22, 0x5a, 0x20, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
-	0x6f, 0x6d, 0x2f, 0x67, 0x6f, 0x2d, 0x6b, 0x72, 0x61, 0x74, 0x6f, 0x73, 0x2f, 0x67, 0x61, 0x74,
-	0x65, 0x77, 0x61, 0x79, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_gateway_proto_rawDesc = []byte{
+	0x0a, 0x0d, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x11, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e,
+	0x76, 0x31, 0x22, 0x58, 0x0a, 0x07, 0x47, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x12, 0x12, 0x0a,
+	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d,
+	0x65, 0x12, 0x39, 0x0a, 0x09, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x73, 0x18, 0x02,
+	0x20, 0x03, 0x28, 0x0b, 0x32, 0x1b, 0x2e, 0x67, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x63,
+	0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2e, 0x76, 0x31, 0x2e, 0x4c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65,
+	0x72, 0x52, 0x09, 0x6c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x73, 0x22, 0x24, 0x0a, 0x08,
+	0x4c, 0x69, 0x73, 0x74, 0x65, 0x6e, 0x65, 0x72, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72,
+	0x65, 0x73, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65,
+	0x73, 0x73, 0x42, 0x22, 0x5a, 0x20, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
+	0x2f, 0x67, 0x6f, 0x2d, 0x6b, 0x72, 0x61, 0x74, 0x6f, 0x73, 0x2f, 0x67, 0x61, 0x74, 0x65, 0x77,
+	0x61, 0x79, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_gateway_config_v1_gateway_proto_rawDescOnce sync.Once
-	file_gateway_config_v1_gateway_proto_rawDescData = file_gateway_config_v1_gateway_proto_rawDesc
+	file_gateway_proto_rawDescOnce sync.Once
+	file_gateway_proto_rawDescData = file_gateway_proto_rawDesc
 )
 
-func file_gateway_config_v1_gateway_proto_rawDescGZIP() []byte {
-	file_gateway_config_v1_gateway_proto_rawDescOnce.Do(func() {
-		file_gateway_config_v1_gateway_proto_rawDescData = protoimpl.X.CompressGZIP(file_gateway_config_v1_gateway_proto_rawDescData)
+func file_gateway_proto_rawDescGZIP() []byte {
+	file_gateway_proto_rawDescOnce.Do(func() {
+		file_gateway_proto_rawDescData = protoimpl.X.CompressGZIP(file_gateway_proto_rawDescData)
 	})
-	return file_gateway_config_v1_gateway_proto_rawDescData
+	return file_gateway_proto_rawDescData
 }
 
-var file_gateway_config_v1_gateway_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_gateway_config_v1_gateway_proto_goTypes = []interface{}{
+var file_gateway_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_gateway_proto_goTypes = []interface{}{
 	(*Gateway)(nil),  // 0: gateway.config.v1.Gateway
 	(*Listener)(nil), // 1: gateway.config.v1.Listener
 }
-var file_gateway_config_v1_gateway_proto_depIdxs = []int32{
+var file_gateway_proto_depIdxs = []int32{
 	1, // 0: gateway.config.v1.Gateway.listeners:type_name -> gateway.config.v1.Listener
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
@@ -167,13 +166,13 @@ var file_gateway_config_v1_gateway_proto_depIdxs = []int32{
 	0, // [0:1] is the sub-list for field type_name
 }
 
-func init() { file_gateway_config_v1_gateway_proto_init() }
-func file_gateway_config_v1_gateway_proto_init() {
-	if File_gateway_config_v1_gateway_proto != nil {
+func init() { file_gateway_proto_init() }
+func file_gateway_proto_init() {
+	if File_gateway_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_gateway_config_v1_gateway_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_gateway_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Gateway); i {
 			case 0:
 				return &v.state
@@ -185,7 +184,7 @@ func file_gateway_config_v1_gateway_proto_init() {
 				return nil
 			}
 		}
-		file_gateway_config_v1_gateway_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_gateway_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Listener); i {
 			case 0:
 				return &v.state
@@ -202,18 +201,18 @@ func file_gateway_config_v1_gateway_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_gateway_config_v1_gateway_proto_rawDesc,
+			RawDescriptor: file_gateway_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_gateway_config_v1_gateway_proto_goTypes,
-		DependencyIndexes: file_gateway_config_v1_gateway_proto_depIdxs,
-		MessageInfos:      file_gateway_config_v1_gateway_proto_msgTypes,
+		GoTypes:           file_gateway_proto_goTypes,
+		DependencyIndexes: file_gateway_proto_depIdxs,
+		MessageInfos:      file_gateway_proto_msgTypes,
 	}.Build()
-	File_gateway_config_v1_gateway_proto = out.File
-	file_gateway_config_v1_gateway_proto_rawDesc = nil
-	file_gateway_config_v1_gateway_proto_goTypes = nil
-	file_gateway_config_v1_gateway_proto_depIdxs = nil
+	File_gateway_proto = out.File
+	file_gateway_proto_rawDesc = nil
+	file_gateway_proto_goTypes = nil
+	file_gateway_proto_depIdxs = nil
 }
