4abfbed4fb1dedb9a067f4d01a3ad16918321932
chenzhihui <zhihui_chen@foxmail.com>
2021-10-04 14:56:22 +0800

    add proxy factory

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,20 +1,43 @@
 package proxy
 
 import (
-	"fmt"
 	"net/http"
 	"sync/atomic"
 
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
 	"github.com/go-kratos/gateway/client"
-	"github.com/go-kratos/gateway/middleware/cors"
 	"github.com/go-kratos/gateway/router"
 	"github.com/go-kratos/gateway/router/mux"
 )
 
+// ClientFactory is returns service client.
+type ClientFactory func(service *config.Service) (client.Client, error)
+
+// MiddlewareFactory is returns middleware handler.
+type MiddlewareFactory func(ms []*config.Middleware, handler http.Handler) (http.Handler, error)
+
+// Option is proxy option func.
+type Option func(*Proxy)
+
+// WithClientFactory with client factory.
+func WithClientFactory(f ClientFactory) Option {
+	return func(o *Proxy) {
+		o.clientFactory = f
+	}
+}
+
+// WithMiddewareFactory with middleware factory.
+func WithMiddewareFactory(f MiddlewareFactory) Option {
+	return func(o *Proxy) {
+		o.middlewareFactory = f
+	}
+}
+
 // Proxy is a gateway proxy.
 type Proxy struct {
-	router atomic.Value
+	router            atomic.Value
+	clientFactory     ClientFactory
+	middlewareFactory MiddlewareFactory
 }
 
 // New new a gateway proxy.
@@ -24,36 +47,16 @@ func New() (*Proxy, error) {
 	return p, nil
 }
 
-func (p *Proxy) buildClient(service *config.Service) (client.Client, error) {
-	caller, err := client.NewClient(service)
-	if err != nil {
-		return nil, err
-	}
-	return caller, nil
-}
-
-func (p *Proxy) buildMiddleware(ms []*config.Middleware, handler http.Handler) (http.Handler, error) {
-	for _, m := range ms {
-		switch m.Name {
-		case cors.Name:
-			handler = cors.Middleware(m)(handler)
-		default:
-			return nil, fmt.Errorf("not found middleware: %s", m.Name)
-		}
-	}
-	return handler, nil
-}
-
 // Update updates service endpoint.
 func (p *Proxy) Update(services []*config.Service) error {
 	router := mux.NewRouter()
 	for _, s := range services {
-		caller, err := p.buildClient(s)
+		caller, err := p.clientFactory(s)
 		if err != nil {
 			return err
 		}
 		for _, e := range s.Endpoints {
-			handler, err := p.buildMiddleware(e.Middlewares, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			handler, err := p.middlewareFactory(e.Middlewares, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 				caller.Invoke(w, req)
 			}))
 			if err != nil {
