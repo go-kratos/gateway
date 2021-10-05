4e6a06e666959ccee42bafddbc672995e1047f00
chenzhihui <zhihui_chen@foxmail.com>
2021-10-05 13:55:30 +0800

    add manager api

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,13 @@
 package manager
+
+// Manager is a proxy manager.
+type Manager interface {
+	// gateway
+	AddGateway() error
+	DelGateway() error
+	ListGateway() error
+	// service
+	AddService() error
+	DelService() error
+	ListService() error
+}
