350e4d2979cc4af6bd78386d7901d5a4c1db3f0c
longXboy <longxboyhi@gmail.com>
2021-10-13 23:01:57 +0800

    fix cors

--- endpoint/cors/cors.go
+++ endpoint/cors/cors.go
@@ -2,53 +2,359 @@ package cors
 
 import (
 	"context"
-	"time"
+	"net/http"
+	"strconv"
+	"strings"
 
-	config "github.com/go-kratos/gateway/api/gateway/config/v1"
-	v1 "github.com/go-kratos/gateway/api/gateway/middleware/cors/v1"
 	"github.com/go-kratos/gateway/endpoint"
-	"github.com/gorilla/handlers"
-	"github.com/pkg/errors"
 )
 
-// Name is the middleware name.
-const Name = "cors"
+// CORSOption represents a functional option for configuring the CORS middleware.
+type CORSOption func(*cors) error
 
-// Middleware automatically sets the allow response header.
-func Middleware(cfg *config.Middleware) (endpoint.Middleware, error) {
-	options := &v1.Cors{}
-	if err := cfg.Options.UnmarshalTo(options); err != nil {
-		return nil, errors.WithStack(err)
-	}
+type cors struct {
+	h                      endpoint.Endpoint
+	allowedHeaders         []string
+	allowedMethods         []string
+	allowedOrigins         []string
+	allowedOriginValidator OriginValidator
+	exposedHeaders         []string
+	maxAge                 int
+	ignoreOptions          bool
+	allowCredentials       bool
+	optionStatusCode       int
+}
 
-	opts := make([]handlers.CORSOption, 0, 6)
-	if len(options.AllowedHeaders) > 0 {
-		opts = append(opts, handlers.AllowedHeaders(options.AllowedHeaders))
+// OriginValidator takes an origin string and returns whether or not that origin is allowed.
+type OriginValidator func(string) bool
+
+var (
+	defaultCorsOptionStatusCode = 200
+	defaultCorsMethods          = []string{"GET", "HEAD", "POST"}
+	defaultCorsHeaders          = []string{"Accept", "Accept-Language", "Content-Language", "Origin"}
+	// (WebKit/Safari v9 sends the Origin header by default in AJAX requests)
+)
+
+const (
+	corsOptionMethod           string = "OPTIONS"
+	corsAllowOriginHeader      string = "Access-Control-Allow-Origin"
+	corsExposeHeadersHeader    string = "Access-Control-Expose-Headers"
+	corsMaxAgeHeader           string = "Access-Control-Max-Age"
+	corsAllowMethodsHeader     string = "Access-Control-Allow-Methods"
+	corsAllowHeadersHeader     string = "Access-Control-Allow-Headers"
+	corsAllowCredentialsHeader string = "Access-Control-Allow-Credentials"
+	corsRequestMethodHeader    string = "Access-Control-Request-Method"
+	corsRequestHeadersHeader   string = "Access-Control-Request-Headers"
+	corsOriginHeader           string = "Origin"
+	corsVaryHeader             string = "Vary"
+	corsOriginMatchAll         string = "*"
+)
+
+func (ch *cors) Handle(ctx context.Context, r endpoint.Request) (reply endpoint.Response, err error) {
+	origin := r.Header().Get(corsOriginHeader)
+	if !ch.isOriginAllowed(origin) {
+		if r.Method() != corsOptionMethod || ch.ignoreOptions {
+			return ch.h(ctx, r)
+		}
+
+		return newResponse(200), nil
 	}
-	if len(options.AllowedMethods) > 0 {
-		opts = append(opts, handlers.AllowedMethods(options.AllowedMethods))
-	}
-	if len(options.AllowedOrigins) > 0 {
-		opts = append(opts, handlers.AllowedOrigins(options.AllowedOrigins))
-	}
-	if len(options.ExposedHeaders) > 0 {
-		opts = append(opts, handlers.ExposedHeaders(options.ExposedHeaders))
-	}
-	if options.MaxAge != nil {
-		maxAge := int(options.MaxAge.AsDuration() / time.Second)
-		if maxAge > 0 {
-			opts = append(opts, handlers.MaxAge(maxAge))
+	header := make(http.Header)
+	if r.Method() == corsOptionMethod {
+		if ch.ignoreOptions {
+			return ch.h(ctx, r)
+		}
+
+		if _, ok := r.Header()[corsRequestMethodHeader]; !ok {
+			return newResponse(http.StatusBadRequest), nil
+		}
+
+		method := r.Header().Get(corsRequestMethodHeader)
+		if !ch.isMatch(method, ch.allowedMethods) {
+			return newResponse(http.StatusMethodNotAllowed), nil
+		}
+
+		requestHeaders := strings.Split(r.Header().Get(corsRequestHeadersHeader), ",")
+		allowedHeaders := []string{}
+		for _, v := range requestHeaders {
+			canonicalHeader := http.CanonicalHeaderKey(strings.TrimSpace(v))
+			if canonicalHeader == "" || ch.isMatch(canonicalHeader, defaultCorsHeaders) {
+				continue
+			}
+
+			if !ch.isMatch(canonicalHeader, ch.allowedHeaders) {
+				return newResponse(http.StatusForbidden), nil
+			}
+
+			allowedHeaders = append(allowedHeaders, canonicalHeader)
+		}
+
+		if len(allowedHeaders) > 0 {
+			header.Set(corsAllowHeadersHeader, strings.Join(allowedHeaders, ","))
+		}
+
+		if ch.maxAge > 0 {
+			header.Set(corsMaxAgeHeader, strconv.Itoa(ch.maxAge))
+		}
+
+		if !ch.isMatch(method, defaultCorsMethods) {
+			header.Set(corsAllowMethodsHeader, method)
+		}
+	} else {
+		if len(ch.exposedHeaders) > 0 {
+			header.Set(corsExposeHeadersHeader, strings.Join(ch.exposedHeaders, ","))
 		}
 	}
-	if options.AllowCredentials {
-		opts = append(opts, handlers.AllowCredentials())
+
+	if ch.allowCredentials {
+		header.Set(corsAllowCredentialsHeader, "true")
 	}
 
-	//corsMiddleware := handlers.CORS(opts...)
+	if len(ch.allowedOrigins) > 1 {
+		header.Set(corsVaryHeader, corsOriginHeader)
+	}
 
-	return func(handler endpoint.Endpoint) endpoint.Endpoint {
-		return func(ctx context.Context, req endpoint.Request) (reply endpoint.Response, err error) {
-			return handler(ctx, req)
+	returnOrigin := origin
+	if ch.allowedOriginValidator == nil && len(ch.allowedOrigins) == 0 {
+		returnOrigin = "*"
+	} else {
+		for _, o := range ch.allowedOrigins {
+			// A configuration of * is different than explicitly setting an allowed
+			// origin. Returning arbitrary origin headers in an access control allow
+			// origin header is unsafe and is not required by any use case.
+			if o == corsOriginMatchAll {
+				returnOrigin = "*"
+				break
+			}
 		}
-	}, nil
+	}
+	header.Set(corsAllowOriginHeader, returnOrigin)
+
+	if r.Method() == corsOptionMethod {
+		resp := newResponse(ch.optionStatusCode, header)
+		return resp, nil
+	}
+	resp, err := ch.h(ctx, r)
+	if err == nil {
+		for k, v := range header {
+			resp.Header()[k] = v
+		}
+	}
+	return resp, err
+}
+
+// CORS provides Cross-Origin Resource Sharing middleware.
+// Example:
+//
+//  import (
+//      "net/http"
+//
+//      "github.com/gorilla/handlers"
+//      "github.com/gorilla/mux"
+//  )
+//
+//  func main() {
+//      r := mux.NewRouter()
+//      r.HandleFunc("/users", UserEndpoint)
+//      r.HandleFunc("/projects", ProjectEndpoint)
+//
+//      // Apply the CORS middleware to our top-level router, with the defaults.
+//      http.ListenAndServe(":8000", handlers.CORS()(r))
+//  }
+//
+func CORS(opts ...CORSOption) func(endpoint.Endpoint) endpoint.Endpoint {
+	ch := parseCORSOptions(opts...)
+	return func(h endpoint.Endpoint) endpoint.Endpoint {
+		ch.h = h
+		return ch.Handle
+	}
+}
+
+func parseCORSOptions(opts ...CORSOption) *cors {
+	ch := &cors{
+		allowedMethods:   defaultCorsMethods,
+		allowedHeaders:   defaultCorsHeaders,
+		allowedOrigins:   []string{},
+		optionStatusCode: defaultCorsOptionStatusCode,
+	}
+
+	for _, option := range opts {
+		option(ch)
+	}
+
+	return ch
+}
+
+//
+// Functional options for configuring CORS.
+//
+
+// AllowedHeaders adds the provided headers to the list of allowed headers in a
+// CORS request.
+// This is an append operation so the headers Accept, Accept-Language,
+// and Content-Language are always allowed.
+// Content-Type must be explicitly declared if accepting Content-Types other than
+// application/x-www-form-urlencoded, multipart/form-data, or text/plain.
+func AllowedHeaders(headers []string) CORSOption {
+	return func(ch *cors) error {
+		for _, v := range headers {
+			normalizedHeader := http.CanonicalHeaderKey(strings.TrimSpace(v))
+			if normalizedHeader == "" {
+				continue
+			}
+
+			if !ch.isMatch(normalizedHeader, ch.allowedHeaders) {
+				ch.allowedHeaders = append(ch.allowedHeaders, normalizedHeader)
+			}
+		}
+
+		return nil
+	}
+}
+
+// AllowedMethods can be used to explicitly allow methods in the
+// Access-Control-Allow-Methods header.
+// This is a replacement operation so you must also
+// pass GET, HEAD, and POST if you wish to support those methods.
+func AllowedMethods(methods []string) CORSOption {
+	return func(ch *cors) error {
+		ch.allowedMethods = []string{}
+		for _, v := range methods {
+			normalizedMethod := strings.ToUpper(strings.TrimSpace(v))
+			if normalizedMethod == "" {
+				continue
+			}
+
+			if !ch.isMatch(normalizedMethod, ch.allowedMethods) {
+				ch.allowedMethods = append(ch.allowedMethods, normalizedMethod)
+			}
+		}
+
+		return nil
+	}
+}
+
+// AllowedOrigins sets the allowed origins for CORS requests, as used in the
+// 'Allow-Access-Control-Origin' HTTP header.
+// Note: Passing in a []string{"*"} will allow any domain.
+func AllowedOrigins(origins []string) CORSOption {
+	return func(ch *cors) error {
+		for _, v := range origins {
+			if v == corsOriginMatchAll {
+				ch.allowedOrigins = []string{corsOriginMatchAll}
+				return nil
+			}
+		}
+
+		ch.allowedOrigins = origins
+		return nil
+	}
+}
+
+// AllowedOriginValidator sets a function for evaluating allowed origins in CORS requests, represented by the
+// 'Allow-Access-Control-Origin' HTTP header.
+func AllowedOriginValidator(fn OriginValidator) CORSOption {
+	return func(ch *cors) error {
+		ch.allowedOriginValidator = fn
+		return nil
+	}
+}
+
+// OptionStatusCode sets a custom status code on the OPTIONS requests.
+// Default behaviour sets it to 200 to reflect best practices. This is option is not mandatory
+// and can be used if you need a custom status code (i.e 204).
+//
+// More informations on the spec:
+// https://fetch.spec.whatwg.org/#cors-preflight-fetch
+func OptionStatusCode(code int) CORSOption {
+	return func(ch *cors) error {
+		ch.optionStatusCode = code
+		return nil
+	}
+}
+
+// ExposedHeaders can be used to specify headers that are available
+// and will not be stripped out by the user-agent.
+func ExposedHeaders(headers []string) CORSOption {
+	return func(ch *cors) error {
+		ch.exposedHeaders = []string{}
+		for _, v := range headers {
+			normalizedHeader := http.CanonicalHeaderKey(strings.TrimSpace(v))
+			if normalizedHeader == "" {
+				continue
+			}
+
+			if !ch.isMatch(normalizedHeader, ch.exposedHeaders) {
+				ch.exposedHeaders = append(ch.exposedHeaders, normalizedHeader)
+			}
+		}
+
+		return nil
+	}
+}
+
+// MaxAge determines the maximum age (in seconds) between preflight requests. A
+// maximum of 10 minutes is allowed. An age above this value will default to 10
+// minutes.
+func MaxAge(age int) CORSOption {
+	return func(ch *cors) error {
+		// Maximum of 10 minutes.
+		if age > 600 {
+			age = 600
+		}
+
+		ch.maxAge = age
+		return nil
+	}
+}
+
+// IgnoreOptions causes the CORS middleware to ignore OPTIONS requests, instead
+// passing them through to the next handler. This is useful when your application
+// or framework has a pre-existing mechanism for responding to OPTIONS requests.
+func IgnoreOptions() CORSOption {
+	return func(ch *cors) error {
+		ch.ignoreOptions = true
+		return nil
+	}
+}
+
+// AllowCredentials can be used to specify that the user agent may pass
+// authentication details along with the request.
+func AllowCredentials() CORSOption {
+	return func(ch *cors) error {
+		ch.allowCredentials = true
+		return nil
+	}
+}
+
+func (ch *cors) isOriginAllowed(origin string) bool {
+	if origin == "" {
+		return false
+	}
+
+	if ch.allowedOriginValidator != nil {
+		return ch.allowedOriginValidator(origin)
+	}
+
+	if len(ch.allowedOrigins) == 0 {
+		return true
+	}
+
+	for _, allowedOrigin := range ch.allowedOrigins {
+		if allowedOrigin == origin || allowedOrigin == corsOriginMatchAll {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (ch *cors) isMatch(needle string, haystack []string) bool {
+	for _, v := range haystack {
+		if v == needle {
+			return true
+		}
+	}
+
+	return false
 }
---
+++ endpoint/cors/response.go
@@ -0,0 +1,38 @@
+package cors
+
+import (
+	"io"
+	"net/http"
+)
+
+type response struct {
+	statusCode int
+	header     http.Header
+}
+
+func (r *response) Header() http.Header {
+	return r.header
+}
+
+func (r *response) Trailer() http.Header {
+	return nil
+}
+
+func (r *response) StatusCode() int {
+	if r.statusCode == 0 {
+		return 200
+	}
+	return r.statusCode
+}
+
+func (r *response) Body() io.ReadCloser {
+	return nil
+}
+
+func NewResponse(statusCode int, headers ...http.Header) *response {
+	var header http.Header
+	if len(headers) == 0 {
+		header = make(http.Header)
+	}
+	return &response{statusCode: statusCode, header: header}
+}
