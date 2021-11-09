7f7c7997f66dffb73cfb72be3cc6d8a62602fd60
realityone <realityone@me.com>
2021-11-09 18:34:00 +0800

    unify client and retry client

--- client/client.go
+++ client/client.go
@@ -7,15 +7,15 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
-	"github.com/go-kratos/gateway/endpoint"
+	"google.golang.org/grpc/codes"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/go-kratos/kratos/v2/selector"
 	"github.com/go-kratos/kratos/v2/selector/wrr"
-	"google.golang.org/grpc/codes"
 )
 
 // Client is a proxy client.
@@ -23,128 +23,142 @@ type Client interface {
 	Invoke(ctx context.Context, req *http.Request) (*http.Response, error)
 }
 
-type client struct {
-	selector selector.Selector
+type clientFactoryContext struct {
+	endpoint  *config.Endpoint
+	logHelper *log.Helper
+	registry  registry.Discovery
 }
 
-func (c *client) Invoke(ctx context.Context, req *http.Request) (*http.Response, error) {
-	opts, _ := endpoint.FromContext(ctx)
-	selected, done, err := c.selector.Select(ctx, selector.WithFilter(opts.Filters...))
-	if err != nil {
-		return nil, err
+func (cfCtx *clientFactoryContext) applyNodes(dst selector.Selector) error {
+	nodes := []selector.Node{}
+	atomicNodes := make(map[string]*node)
+	for _, backend := range cfCtx.endpoint.Backends {
+		target, err := parseTarget(backend.Target)
+		if err != nil {
+			return err
+		}
+		switch target.Scheme {
+		case "direct":
+			node := newNode(backend.Target, cfCtx.endpoint.Protocol, backend.Weight, calcTimeout(cfCtx.endpoint))
+			nodes = append(nodes, node)
+			atomicNodes[backend.Target] = node
+			dst.Apply(nodes)
+		case "discovery":
+			w, err := cfCtx.registry.Watch(context.Background(), target.Endpoint)
+			if err != nil {
+				return err
+			}
+			go func() {
+				// TODO: goroutine leak
+				// only one backend configuration allowed when using service discovery
+				for {
+					services, err := w.Next()
+					if err != nil && errors.Is(err, context.Canceled) {
+						return
+					}
+					if len(services) == 0 {
+						continue
+					}
+					var nodes []selector.Node
+					atomicNodes := make(map[string]*node)
+					for _, ser := range services {
+						scheme := strings.ToLower(cfCtx.endpoint.Protocol.String())
+						addr, err := parseEndpoint(ser.Endpoints, scheme, false)
+						if err != nil {
+							cfCtx.logHelper.Errorf("failed to parse endpoint: %v", err)
+							continue
+						}
+						node := newNode(addr, cfCtx.endpoint.Protocol, backend.Weight, calcTimeout(cfCtx.endpoint))
+						nodes = append(nodes, node)
+						atomicNodes[addr] = node
+					}
+					dst.Apply(nodes)
+				}
+			}()
+		default:
+			return fmt.Errorf("unknown scheme: %s", target.Scheme)
+		}
 	}
-	defer done(ctx, selector.DoneInfo{Err: err})
+	return nil
+}
 
-	node := selected.(*node)
-	req = req.WithContext(ctx)
-	req.URL.Scheme = "http"
-	req.URL.Host = selected.Address()
-	req.RequestURI = ""
+func calcTimeout(endpoint *config.Endpoint) time.Duration {
+	timeout := endpoint.Timeout.AsDuration()
+	if endpoint.Retry == nil {
+		return timeout
+	}
+	if endpoint.Retry.PerTryTimeout != nil &&
+		endpoint.Retry.PerTryTimeout.AsDuration() > 0 &&
+		endpoint.Retry.PerTryTimeout.AsDuration() < timeout {
+		return endpoint.Retry.PerTryTimeout.AsDuration()
+	}
+	return timeout
+}
 
-	return node.client.Do(req)
+func calcAttempts(endpoint *config.Endpoint) uint32 {
+	if endpoint.Retry == nil {
+		return 1
+	}
+	if endpoint.Retry.Attempts == 0 {
+		return 1
+	}
+	return endpoint.Retry.Attempts
+}
+
+func parseRetryConditon(endpoint *config.Endpoint) ([][]uint32, error) {
+	if endpoint.Retry == nil {
+		return [][]uint32{}, nil
+	}
+	conditions := make([][]uint32, 0, len(endpoint.Retry.Conditions))
+	for _, condition := range endpoint.Retry.Conditions {
+		var statusCode []uint32
+		switch endpoint.Protocol {
+		case config.Protocol_GRPC:
+			var code codes.Code
+			if err := code.UnmarshalJSON([]byte(strings.ToUpper(condition))); err != nil {
+				return nil, err
+			}
+			statusCode = append(statusCode, uint32(code))
+		case config.Protocol_HTTP:
+			cs := strings.Split(condition, "-")
+			if len(cs) == 0 || len(cs) > 2 {
+				return nil, fmt.Errorf("invalid condition %s", condition)
+			}
+			for _, c := range cs {
+				code, err := strconv.ParseUint(c, 10, 16)
+				if err != nil {
+					return nil, err
+				}
+				statusCode = append(statusCode, uint32(code))
+			}
+		}
+		conditions = append(conditions, statusCode)
+	}
+	return conditions, nil
 }
 
 // NewFactory new a client factory.
 func NewFactory(logger log.Logger, r registry.Discovery) func(endpoint *config.Endpoint) (Client, error) {
 	log := log.NewHelper(logger)
 	return func(endpoint *config.Endpoint) (Client, error) {
-		var c Client
-		timeout := endpoint.Timeout.AsDuration()
 		wrr := wrr.New()
-
-		if endpoint.Retry != nil && endpoint.Retry.Attempts > 1 {
-			if endpoint.Retry.PerTryTimeout != nil && endpoint.Retry.PerTryTimeout.AsDuration() > 0 && endpoint.Retry.PerTryTimeout.AsDuration() < timeout {
-				timeout = endpoint.Retry.PerTryTimeout.AsDuration()
-			}
-			rc := &retryClient{
-				selector: wrr,
-				attempts: 1,
-				protocol: endpoint.Protocol,
-			}
-			rc.attempts = endpoint.Retry.Attempts
-			for _, condition := range endpoint.Retry.Conditions {
-				var statusCode []uint32
-				if endpoint.Protocol == config.Protocol_GRPC {
-					var code codes.Code
-					err := code.UnmarshalJSON([]byte(strings.ToUpper(condition)))
-					if err != nil {
-						return nil, err
-					}
-					statusCode = append(statusCode, uint32(code))
-				} else {
-					cs := strings.Split(condition, "-")
-					if len(cs) == 0 || len(cs) > 2 {
-						return nil, fmt.Errorf("invalid condition %s", condition)
-					}
-					for _, c := range cs {
-						code, err := strconv.ParseUint(c, 10, 16)
-						if err != nil {
-							return nil, err
-						}
-						statusCode = append(statusCode, uint32(code))
-					}
-				}
-				rc.conditions = append(rc.conditions, statusCode)
-			}
-			c = rc
-		} else {
-			if endpoint.Retry != nil && endpoint.Retry.PerTryTimeout != nil && endpoint.Retry.PerTryTimeout.AsDuration() > 0 && endpoint.Retry.PerTryTimeout.AsDuration() < timeout {
-				timeout = endpoint.Retry.PerTryTimeout.AsDuration()
-			}
-			c = &client{
-				selector: wrr,
-			}
+		factoryCtx := &clientFactoryContext{
+			endpoint:  endpoint,
+			logHelper: log,
+			registry:  r,
 		}
+		factoryCtx.applyNodes(wrr)
 
-		nodes := []selector.Node{}
-		atomicNodes := make(map[string]*node, 0)
-		for _, backend := range endpoint.Backends {
-			target, err := parseTarget(backend.Target)
-			if err != nil {
-				return nil, err
-			}
-			switch target.Scheme {
-			case "direct":
-				node := newNode(backend.Target, endpoint.Protocol, backend.Weight, timeout)
-				nodes = append(nodes, node)
-				atomicNodes[backend.Target] = node
-			case "discovery":
-				w, err := r.Watch(context.Background(), target.Endpoint)
-				if err != nil {
-					return nil, err
-				}
-				go func() {
-					// TODO: goroutine leak
-					// only one backend configuration allowed when using service discovery
-					for {
-						services, err := w.Next()
-						if err != nil && errors.Is(err, context.Canceled) {
-							return
-						}
-						if len(services) == 0 {
-							continue
-						}
-						var nodes []selector.Node
-						atomicNodes := make(map[string]*node, 0)
-						for _, ser := range services {
-							scheme := strings.ToLower(endpoint.Protocol.String())
-							addr, err := parseEndpoint(ser.Endpoints, scheme, false)
-							if err != nil {
-								log.Errorf("failed to parse endpoint: %v", err)
-								continue
-							}
-							node := newNode(addr, endpoint.Protocol, backend.Weight, timeout)
-							nodes = append(nodes, node)
-							atomicNodes[addr] = node
-						}
-						wrr.Apply(nodes)
-					}
-				}()
-			default:
-				return nil, fmt.Errorf("unknown scheme: %s", target.Scheme)
-			}
+		client := &retryClient{
+			selector: wrr,
+			attempts: calcAttempts(endpoint),
+			protocol: endpoint.Protocol,
 		}
-		wrr.Apply(nodes)
-		return c, nil
+		retryCond, err := parseRetryConditon(endpoint)
+		if err != nil {
+			return nil, err
+		}
+		client.conditions = retryCond
+		return client, nil
 	}
 }
