d802d6b3fd029eec74c9274746cfdcb257e6d154
chenzhihui <zhihui_chen@foxmail.com>
2021-11-16 20:21:30 +0800

    add h2c example

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -4,11 +4,26 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
-	http.HandleFunc("/helloworld/foo", func(w http.ResponseWriter, req *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/helloworld/foo", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "bar")
 	})
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	srv := &http.Server{
+		Addr: ":8000",
+		Handler: h2c.NewHandler(mux, &http2.Server{
+			IdleTimeout: time.Second * 120,
+		}),
+		ReadTimeout:       time.Second * 1,
+		ReadHeaderTimeout: time.Second * 1,
+		WriteTimeout:      time.Second * 1,
+		IdleTimeout:       time.Second * 120,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
