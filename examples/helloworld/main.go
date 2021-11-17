df6637bad9b140d3496649cc87785addab1e9269
chenzhihui <zhihui_chen@foxmail.com>
2021-11-17 13:28:31 +0800

    add bind addr

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net/http"
 )
 
+var bind string
+
+func init() {
+	flag.StringVar(&bind, "bind", ":8000", "server address, eg: 127.0.0.1:8000")
+}
+
 func main() {
+	flag.Parse()
 	http.HandleFunc("/helloworld/foo", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "bar")
 	})
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Println("listening on:", bind)
+	log.Fatal(http.ListenAndServe(bind, nil))
 }
