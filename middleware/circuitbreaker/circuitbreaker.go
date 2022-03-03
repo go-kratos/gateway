6bea5d29cb3e1f8a76745157d38b66daf7d7800f
realityone <realityone@me.com>
2022-03-03 14:41:33 +0800

    success response condition

--- middleware/circuitbreaker/circuitbreaker.go
+++ middleware/circuitbreaker/circuitbreaker.go
@@ -2,19 +2,100 @@ package circuitbreaker
 
 import (
 	"context"
+	"math/rand"
 	"net/http"
+	"sync"
 
+	"github.com/go-kratos/aegis/circuitbreaker"
 	"github.com/go-kratos/aegis/circuitbreaker/sre"
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
 	v1 "github.com/go-kratos/gateway/api/gateway/middleware/circuitbreaker/v1"
 	"github.com/go-kratos/gateway/client"
 	"github.com/go-kratos/gateway/middleware"
+	"github.com/go-kratos/gateway/proxy/condition"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func Init(in middleware.Factory) {
 	middleware.Register("circuitbreaker", in)
+}
+
+var (
+	_ circuitbreaker.CircuitBreaker = &ratioTrigger{}
+)
+
+type ratioTrigger struct {
+	*v1.CircuitBreaker_Ratio
+	lock sync.Mutex
+	rand *rand.Rand
+}
+
+func newRatioTrigger(in *v1.CircuitBreaker_Ratio) *ratioTrigger {
+	return &ratioTrigger{
+		CircuitBreaker_Ratio: in,
+		rand:                 rand.New(rand.NewSource(rand.Int63())),
+	}
+}
+
+func (r *ratioTrigger) Allow() error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	if r.rand.Int63n(10000) < r.Ratio {
+		return nil
+	}
+	return circuitbreaker.ErrNotAllowed
+}
+func (*ratioTrigger) MarkSuccess() {}
+func (*ratioTrigger) MarkFailed()  {}
+
+type nopTrigger struct{}
+
+func (nopTrigger) Allow() error { return nil }
+func (nopTrigger) MarkSuccess() {}
+func (nopTrigger) MarkFailed()  {}
+
+func makeBreakerTrigger(in *v1.CircuitBreaker) circuitbreaker.CircuitBreaker {
+	switch trigger := in.Trigger.(type) {
+	case *v1.CircuitBreaker_SuccessRatio:
+		opts := []sre.Option{}
+		if trigger.SuccessRatio.Bucket != 0 {
+			opts = append(opts, sre.WithBucket(int(trigger.SuccessRatio.Bucket)))
+		}
+		if trigger.SuccessRatio.Request != 0 {
+			opts = append(opts, sre.WithRequest(trigger.SuccessRatio.Request))
+		}
+		if trigger.SuccessRatio.Success != 0 {
+			opts = append(opts, sre.WithSuccess(trigger.SuccessRatio.Success))
+		}
+		if trigger.SuccessRatio.Window != nil {
+			opts = append(opts, sre.WithWindow(trigger.SuccessRatio.Window.AsDuration()))
+		}
+		return sre.NewBreaker(opts...)
+	case *v1.CircuitBreaker_Ratio:
+		return newRatioTrigger(trigger)
+	default:
+		return nopTrigger{}
+	}
+}
+
+func makeOnBreakHandler(in *v1.CircuitBreaker, factory client.Factory) (middleware.Handler, error) {
+	switch action := in.Action.(type) {
+	case *v1.CircuitBreaker_BackupService:
+		client, err := factory(action.BackupService.Endpoint)
+		if err != nil {
+			return nil, err
+		}
+		return client.Do, nil
+	default:
+		return func(context.Context, *http.Request) (*http.Response, error) {
+			return nil, circuitbreaker.ErrNotAllowed
+		}, nil
+	}
+}
+
+func isSuccessResponse(conditions []condition.Condition, resp *http.Response) bool {
+	return condition.JudgeConditons(conditions, resp, true)
 }
 
 func New(factory client.Factory) middleware.Factory {
@@ -25,18 +106,14 @@ func New(factory client.Factory) middleware.Factory {
 				return nil, err
 			}
 		}
-
-		succRatio := options.GetSuccessRatio()
-		breaker := sre.NewBreaker(
-			sre.WithBucket(int(succRatio.Bucket)),
-			sre.WithRequest(succRatio.Request),
-			sre.WithSuccess(succRatio.Success),
-			sre.WithWindow(succRatio.Window.AsDuration()),
-		)
-
-		client, err := factory(options.GetBackupService().GetEndpoint())
+		breaker := makeBreakerTrigger(options)
+		onBreakHandler, err := makeOnBreakHandler(options, factory)
 		if err != nil {
-			panic(err)
+			return nil, err
+		}
+		assertCondtions, err := condition.ParseConditon(options.AssertCondtions...)
+		if err != nil {
+			return nil, err
 		}
 		return func(handler middleware.Handler) middleware.Handler {
 			return func(ctx context.Context, req *http.Request) (*http.Response, error) {
@@ -45,16 +122,16 @@ func New(factory client.Factory) middleware.Factory {
 					// NOTE: when client reject requets locally,
 					// continue add counter let the drop ratio higher.
 					breaker.MarkFailed()
-					resp, err := client.Do(ctx, req)
-					if err != nil {
-						return nil, err
-					}
-					return resp, nil
+					return onBreakHandler(ctx, req)
 				}
 				resp, err := handler(ctx, req)
 				if err != nil {
 					breaker.MarkFailed()
 					return nil, err
+				}
+				if !isSuccessResponse(assertCondtions, resp) {
+					breaker.MarkFailed()
+					return resp, nil
 				}
 				breaker.MarkSuccess()
 				return resp, nil
--- proxy/retry.go
+++ proxy/retry.go
@@ -1,94 +1,18 @@
 package proxy
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
+	"github.com/go-kratos/gateway/proxy/condition"
 )
 
-type retryCondition interface {
-	prepare() error
-	judge(*http.Response) bool
-}
-
-type byStatusCode struct {
-	*config.RetryCondition_ByStatusCode
-	parsedCodes []int64
-}
-
-func (c *byStatusCode) prepare() error {
-	c.parsedCodes = make([]int64, 0, len(c.ByStatusCode))
-	parts := strings.Split(c.ByStatusCode, "-")
-	if len(parts) == 0 || len(parts) > 2 {
-		return fmt.Errorf("invalid condition %s", c.ByStatusCode)
-	}
-	c.parsedCodes = []int64{}
-	for _, p := range parts {
-		code, err := strconv.ParseInt(p, 10, 16)
-		if err != nil {
-			return err
-		}
-		c.parsedCodes = append(c.parsedCodes, code)
-	}
-	return nil
-}
-
-func (c *byStatusCode) judge(resp *http.Response) bool {
-	if len(c.parsedCodes) == 0 {
-		return false
-	}
-	if len(c.parsedCodes) == 1 {
-		return int64(resp.StatusCode) == c.parsedCodes[0]
-	}
-	return (int64(resp.StatusCode) >= c.parsedCodes[0]) &&
-		(int64(resp.StatusCode) <= c.parsedCodes[1])
-}
-
-type byHeader struct {
-	*config.RetryCondition_ByHeader
-	parsed struct {
-		name   string
-		values map[string]struct{}
-	}
-}
-
-func (c *byHeader) judge(resp *http.Response) bool {
-	v := resp.Header.Get(c.ByHeader.Name)
-	if v == "" {
-		return false
-	}
-	_, ok := c.parsed.values[v]
-	return ok
-}
-
-func (c *byHeader) prepare() error {
-	c.parsed.name = c.ByHeader.Name
-	c.parsed.values = map[string]struct{}{}
-	if strings.HasPrefix(c.ByHeader.Value, "[") {
-		values, err := parseAsStringList(c.ByHeader.Value)
-		if err != nil {
-			return err
-		}
-		for _, v := range values {
-			c.parsed.values[v] = struct{}{}
-		}
-		return nil
-	}
-	c.parsed.values[c.ByHeader.Value] = struct{}{}
-	return nil
-}
-
-func parseAsStringList(in string) ([]string, error) {
-	out := []string{}
-	if err := json.Unmarshal([]byte(in), &out); err != nil {
-		return nil, err
-	}
-	return out, nil
+type retryStrategy struct {
+	attempts      int
+	timeout       time.Duration
+	perTryTimeout time.Duration
+	conditions    []condition.Condition
 }
 
 func calcTimeout(endpoint *config.Endpoint) time.Duration {
@@ -125,44 +49,6 @@ func calcPerTryTimeout(endpoint *config.Endpoint) time.Duration {
 	return perTryTimeout
 }
 
-func parseRetryConditon(endpoint *config.Endpoint) ([]retryCondition, error) {
-	if endpoint.Retry == nil {
-		return []retryCondition{}, nil
-	}
-
-	conditions := make([]retryCondition, 0, len(endpoint.Retry.Conditions))
-	for _, rawCond := range endpoint.Retry.Conditions {
-		switch v := rawCond.Condition.(type) {
-		case *config.RetryCondition_ByHeader:
-			cond := &byHeader{
-				RetryCondition_ByHeader: v,
-			}
-			if err := cond.prepare(); err != nil {
-				return nil, err
-			}
-			conditions = append(conditions, cond)
-		case *config.RetryCondition_ByStatusCode:
-			cond := &byStatusCode{
-				RetryCondition_ByStatusCode: v,
-			}
-			if err := cond.prepare(); err != nil {
-				return nil, err
-			}
-			conditions = append(conditions, cond)
-		default:
-			return nil, fmt.Errorf("unknown condition type: %T", v)
-		}
-	}
-	return conditions, nil
-}
-
-type retryStrategy struct {
-	attempts      int
-	timeout       time.Duration
-	perTryTimeout time.Duration
-	conditions    []retryCondition
-}
-
 func prepareRetryStrategy(e *config.Endpoint) (*retryStrategy, error) {
 	strategy := &retryStrategy{
 		attempts:      calcAttempts(e),
@@ -177,11 +63,13 @@ func prepareRetryStrategy(e *config.Endpoint) (*retryStrategy, error) {
 	return strategy, nil
 }
 
-func judgeRetryRequired(conditions []retryCondition, resp *http.Response) bool {
-	for _, cond := range conditions {
-		if cond.judge(resp) {
-			return true
-		}
+func parseRetryConditon(endpoint *config.Endpoint) ([]condition.Condition, error) {
+	if endpoint.Retry == nil {
+		return []condition.Condition{}, nil
 	}
-	return false
+	return condition.ParseConditon(endpoint.Retry.Conditions...)
+}
+
+func judgeRetryRequired(conditions []condition.Condition, resp *http.Response) bool {
+	return condition.JudgeConditons(conditions, resp, false)
 }
