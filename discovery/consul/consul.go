f10e306d55602e5246234c684811feee865cf0f9
longXboy <longxboyhi@gmail.com>
2022-03-18 14:41:38 +0800

    add consul resolver

--- discovery/consul/consul.go
+++ discovery/consul/consul.go
@@ -1,7 +1,10 @@
 package consul
 
 import (
+	"context"
+	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/go-kratos/gateway/discovery"
 	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
@@ -29,5 +32,37 @@ func New(dsn *url.URL) (registry.Discovery, error) {
 	if err != nil {
 		return nil, err
 	}
-	return consul.New(client), nil
+	return consul.New(client, consul.WithServiceResolver(resolver)), nil
+}
+
+func resolver(_ context.Context, entries []*api.ServiceEntry) []*registry.ServiceInstance {
+	services := make([]*registry.ServiceInstance, 0, len(entries))
+	for _, entry := range entries {
+		var version string
+		for _, tag := range entry.Service.Tags {
+			ss := strings.SplitN(tag, "=", 2)
+			if len(ss) == 2 && ss[0] == "version" {
+				version = ss[1]
+			}
+		}
+		var endpoints []string //nolint:prealloc
+		for scheme, addr := range entry.Service.TaggedAddresses {
+			if scheme == "lan_ipv4" || scheme == "wan_ipv4" || scheme == "lan_ipv6" || scheme == "wan_ipv6" {
+				continue
+			}
+			endpoints = append(endpoints, addr.Address)
+		}
+		if len(endpoints) == 0 && entry.Service.Address != "" && entry.Service.Port != 0 {
+			endpoints = append(endpoints, fmt.Sprintf("http://%s:%d", entry.Service.Address, entry.Service.Port))
+		}
+		services = append(services, &registry.ServiceInstance{
+			ID:        entry.Service.ID,
+			Name:      entry.Service.Service,
+			Metadata:  entry.Service.Meta,
+			Version:   version,
+			Endpoints: endpoints,
+		})
+	}
+
+	return services
 }
