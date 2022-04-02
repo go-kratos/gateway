9e9028e3d4114f5463abed1e2915b549e50cb752
chenzhihui <zhihui_chen@foxmail.com>
2022-04-02 15:40:26 +0800

    fix dial timeout

--- client/node.go
+++ client/node.go
@@ -4,6 +4,8 @@ import (
 	"crypto/tls"
 	"net"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/selector"
 	"golang.org/x/net/http2"
@@ -11,19 +13,35 @@ import (
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
 )
 
-const _globalClientPool = 10
-
 var _ selector.Node = &node{}
 var _globalClient = defaultClient()
 var _globalH2Client = defaultH2Client()
+var _dialTimeout = 200 * time.Millisecond
+
+func init() {
+	var err error
+	if v := os.Getenv("PROXY_DIAL_TIMEOUT"); v != "" {
+		if _dialTimeout, err = time.ParseDuration(v); err != nil {
+			panic(err)
+		}
+	}
+}
 
 func defaultClient() *http.Client {
-	tr := http.DefaultTransport.(*http.Transport).Clone()
-	tr.MaxIdleConns = 1000
-	tr.MaxConnsPerHost = 100
-	tr.MaxIdleConnsPerHost = 100
-	tr.DisableCompression = true
-	return &http.Client{Transport: tr}
+	return &http.Client{Transport: &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   _dialTimeout,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          1000,
+		MaxIdleConnsPerHost:   100,
+		MaxConnsPerHost:       100,
+		DisableCompression:    true,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}}
 }
 
 func defaultH2Client() *http.Client {
@@ -35,7 +53,7 @@ func defaultH2Client() *http.Client {
 			// Pretend we are dialing a TLS endpoint.
 			// Note, we ignore the passed tls.Config
 			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
-				return net.Dial(network, addr)
+				return net.DialTimeout(network, addr, _dialTimeout)
 			},
 		},
 	}
