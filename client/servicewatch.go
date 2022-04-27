ebe1a14da52f6c7e69f75b8a4f9ce5a76acaed48
realityone <realityone@me.com>
2022-04-27 15:20:32 +0800

    discovery node with subset size (#97)
    * discovery node with subset size
    
    * endpoint ha
    
    * subset option
    
    * subset as int32
    
    * put size in subset func
    
    * default subset size
    
    * gen client id by self
    
    * move subset in service watcher
    
    * debug logging
    
    * logging
    
    * subset
    
    * subset size logging
    
    * remove debug logging
    
    * fix subset watcher
    
    * using watch to obtain service instances and cache selected instances
    
    * remove subset selector
    
    * remove subset config
    
    * fix logging
    
    * watch logging
    
    * make sure the service is done the initialize discovery
    
    * initialize succeed logging
    
    * instance set hash
    
    Co-authored-by: chenzhihui <zhihui_chen@foxmail.com>

--- client/servicewatch.go
+++ client/servicewatch.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"hash/crc32"
+	"sort"
+	"strconv"
 	"sync"
 	"time"
 
@@ -12,23 +15,39 @@ import (
 )
 
 var ErrCancelWatch = errors.New("cancel watch")
-
 var globalServiceWatcher = newServiceWatcher()
 
 func uuid4() string {
 	return uuid.NewString()
 }
 
+func instancesSetHash(instances []*registry.ServiceInstance) string {
+	sort.Slice(instances, func(i, j int) bool {
+		return instances[i].ID < instances[j].ID
+	})
+	jsBytes, err := json.Marshal(instances)
+	if err != nil {
+		return ""
+	}
+	return strconv.FormatUint(uint64(crc32.ChecksumIEEE(jsBytes)), 10)
+}
+
+type watcherStatus struct {
+	watcher           registry.Watcher
+	initializedChan   chan struct{}
+	selectedInstances []*registry.ServiceInstance
+}
+
 type serviceWatcher struct {
-	lock     sync.RWMutex
-	watcher  map[string]registry.Watcher
-	callback map[string]map[string]func([]*registry.ServiceInstance) error
+	lock          sync.RWMutex
+	watcherStatus map[string]*watcherStatus
+	callback      map[string]map[string]func([]*registry.ServiceInstance) error
 }
 
 func newServiceWatcher() *serviceWatcher {
 	return &serviceWatcher{
-		watcher:  make(map[string]registry.Watcher),
-		callback: make(map[string]map[string]func([]*registry.ServiceInstance) error),
+		watcherStatus: make(map[string]*watcherStatus),
+		callback:      make(map[string]map[string]func([]*registry.ServiceInstance) error),
 	}
 }
 
@@ -37,40 +56,86 @@ func jsonify(in interface{}) string {
 	return string(bs)
 }
 
+func (s *serviceWatcher) setSelectedCache(endpoint string, instances []*registry.ServiceInstance) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.watcherStatus[endpoint].selectedInstances = instances
+}
+
+func (s *serviceWatcher) getSelectedCache(endpoint string) ([]*registry.ServiceInstance, bool) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	ws, ok := s.watcherStatus[endpoint]
+	if ok {
+		return ws.selectedInstances, true
+	}
+	return nil, false
+}
+
 func (s *serviceWatcher) Add(ctx context.Context, discovery registry.Discovery, endpoint string, callback func([]*registry.ServiceInstance) error) (watcherExisted bool) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	LOG.Infof("Add watcher on endpoint: %s", endpoint)
 	existed := func() bool {
-		ctx, cancel := context.WithTimeout(ctx, time.Second*15)
-		defer cancel()
-		services, err := discovery.GetService(ctx, endpoint)
-		if err != nil {
-			LOG.Errorf("Failed to do initial services discovery on endpoint: %s, err: %+v, starting with empty service instance", endpoint, err)
-		}
-		LOG.Infof("Initialize services discovery on endpoint: %s, services: %s", endpoint, jsonify(services))
-		callback(services)
+		ws, ok := s.watcherStatus[endpoint]
+		if ok {
+			// this channel is used to notify the caller that the service watcher is initialized and ready to use
+			<-ws.initializedChan
 
-		if _, ok := s.watcher[endpoint]; ok {
+			if len(ws.selectedInstances) > 0 {
+				LOG.Infof("Using cached %d selected instances on endpoint: %s, hash: %s", len(ws.selectedInstances), endpoint, instancesSetHash(ws.selectedInstances))
+				callback(ws.selectedInstances)
+				return true
+			}
+
 			return true
+		}
+
+		ws = &watcherStatus{
+			initializedChan: make(chan struct{}),
 		}
 		watcher, err := discovery.Watch(ctx, endpoint)
 		if err != nil {
 			LOG.Errorf("Failed to initialize watcher on endpoint: %s, err: %+v", endpoint, err)
 			return false
 		}
-		s.watcher[endpoint] = watcher
+		LOG.Infof("Succeeded to initialize watcher on endpoint: %s", endpoint)
+		ws.watcher = watcher
+		s.watcherStatus[endpoint] = ws
+
+		func() {
+			defer close(ws.initializedChan)
+			LOG.Infof("Starting to do initialize services discovery on endpoint: %s", endpoint)
+			services, err := watcher.Next()
+			if err != nil {
+				LOG.Errorf("Failed to do initialize services discovery on endpoint: %s, err: %+v, the watch process will attempt asynchronously", endpoint, err)
+				return
+			}
+			LOG.Infof("Succeeded to do initialize services discovery on endpoint: %s, %d services, hash: %s", endpoint, len(services), instancesSetHash(ws.selectedInstances))
+			ws.selectedInstances = services
+			callback(services)
+		}()
 
 		go func() {
 			for {
 				services, err := watcher.Next()
-				if err != nil && errors.Is(err, context.Canceled) {
-					return
-				}
-				if len(services) == 0 {
+				if err != nil {
+					if errors.Is(err, context.Canceled) {
+						LOG.Warnf("The watch process on: %s has been canceled", endpoint)
+						return
+					}
+					LOG.Errorf("Failed to watch on endpoint: %s, err: %+v, the watch process will attempt again after 1 second", endpoint, err)
+					time.Sleep(time.Second)
 					continue
 				}
+				if len(services) == 0 {
+					LOG.Warnf("Empty services on endpoint: %s, this most likely no available instance in discovery", endpoint)
+					continue
+				}
+				LOG.Infof("Received %d services on endpoint: %s, hash: %s", len(services), endpoint, instancesSetHash(services))
+				s.setSelectedCache(endpoint, services)
 				s.doCallback(endpoint, services)
 			}
 		}()
@@ -91,7 +156,6 @@ func (s *serviceWatcher) Add(ctx context.Context, discovery registry.Discovery, 
 
 func (s *serviceWatcher) doCallback(endpoint string, services []*registry.ServiceInstance) {
 	cleanup := []string{}
-
 	func() {
 		s.lock.RLock()
 		defer s.lock.RUnlock()
@@ -110,7 +174,7 @@ func (s *serviceWatcher) doCallback(endpoint string, services []*registry.Servic
 	if len(cleanup) <= 0 {
 		return
 	}
-	LOG.Infof("Cleanup callback on endpoint: %q with key: %+v", endpoint, cleanup)
+	LOG.Infof("Cleanup callback on endpoint: %q with keys: %+v", endpoint, cleanup)
 	func() {
 		s.lock.Lock()
 		defer s.lock.Unlock()
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/go-kratos/gateway
 go 1.15
 
 require (
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/go-kratos/aegis v0.1.1
 	github.com/go-kratos/kratos/contrib/registry/consul/v2 v2.0.0-20220318065833-e66a2905ab70
 	github.com/go-kratos/kratos/v2 v2.2.1
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/consul/api v1.12.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/prometheus/client_golang v1.12.1
 	go.opentelemetry.io/otel v1.4.1
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.4.1
@@ -20,5 +24,6 @@ require (
 	google.golang.org/grpc v1.44.0
 	google.golang.org/grpc/examples v0.0.0-20220223153006-a73725f42db9
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	sigs.k8s.io/yaml v1.3.0
 )
