a9a9d96e5dac6f51d5f91c75ff6ef6e3f9e94115
ASkyFullOfStars <56948889+ASkyFullOfStar@users.noreply.github.com>
2022-05-23 18:17:03 +0800

    Update rewrite.go

--- middleware/rewrite/rewrite.go
+++ middleware/rewrite/rewrite.go
@@ -1,11 +1,9 @@
 package rewrite
 
 import (
-	"net/http"
-	"strings"
-
 	config "github.com/go-kratos/gateway/api/gateway/config/v1"
 	v1 "github.com/go-kratos/gateway/api/gateway/middleware/rewrite/v1"
+	"net/http"
 
 	"github.com/go-kratos/gateway/middleware"
 	"google.golang.org/protobuf/proto"
@@ -17,42 +15,75 @@ func init() {
 }
 
 func Middleware(c *config.Middleware) (middleware.Middleware, error) {
-	options := &v1.ReplacePath{}
+	options := &v1.Rewrite{}
 	if c.Options != nil {
 		if err := anypb.UnmarshalTo(c.Options, options, proto.UnmarshalOptions{Merge: true}); err != nil {
 			return nil, err
 		}
 	}
-	pathBuilder := func(req *http.Request) string {
-		return options.DestPath
-	}
-	if len(options.PathParam) > 0 {
-		pathBuilder = func(req *http.Request) string {
-			out := options.DestPath
-			for key, keySchema := range options.PathParam {
-				switch keySchema.Type {
-				case v1.KeySchemaType_QUERY:
-					query := req.URL.Query()
-					out = strings.ReplaceAll(options.DestPath, "{"+key+"}", query.Get(keySchema.Key))
-					if keySchema.Strip {
-						query.Del(keySchema.Key)
-						req.URL.RawQuery = query.Encode()
+	return func(next http.RoundTripper) http.RoundTripper {
+		return middleware.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+			if options.HostRewrite != nil {
+				req.Host = *options.HostRewrite
+			}
+			if options.PathRewrite != nil {
+				req.URL.Path = *options.PathRewrite
+			}
+			requestHeadersRewrite := options.ReponseHeadersRewrite
+
+			if requestHeadersRewrite != nil {
+				if len(options.GetRequestHeadersRewrite().Set) > 0 {
+					for key, value := range options.GetRequestHeadersRewrite().Set {
+						req.Header.Set(key, value)
+					}
+				}
+				if len(options.GetRequestHeadersRewrite().Add) > 0 {
+					for key, value := range options.GetRequestHeadersRewrite().Add {
+						if req.Header.Get(key) == ""{
+							req.Header.Add(key, value)
+						}else {
+							req.Header.Set(key, value)
+						}
+					}
+				}
+				if len(options.GetRequestHeadersRewrite().Remove) > 0 {
+					for _, value := range options.GetRequestHeadersRewrite().Remove {
+						if req.Header.Get(value) != "" {
+							req.Header.Del(value)
+						}
 					}
 				}
 			}
-			return out
-		}
-	}
 
-	header := &v1.Header{}
-	return func(next http.RoundTripper) http.RoundTripper {
-		return middleware.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
-			dstPath := pathBuilder(req)
-			req.URL.Path = dstPath
-			for key, value := range header.HttpHeaders {
-				req.Header.Set(key, value)
+			resp, err := next.RoundTrip(req)
+			if err != nil {
+				return nil, err
 			}
-			return next.RoundTrip(req)
+			respondHeadersRewrite := options.ReponseHeadersRewrite
+			if respondHeadersRewrite != nil {
+				if len(respondHeadersRewrite.Set) > 0 {
+					for key, value := range options.GetRequestHeadersRewrite().Set {
+						resp.Header.Set(key, value)
+					}
+				}
+				if len(options.GetReponseHeadersRewrite().Add) > 0 {
+					for key, value := range options.GetRequestHeadersRewrite().Add {
+						if resp.Header.Get(key) == ""{
+							req.Header.Add(key, value)
+						}else {
+							resp.Header.Set(key, value)
+						}
+					}
+				}
+				if len(options.GetReponseHeadersRewrite().Remove) > 0 {
+					for _, value := range options.GetRequestHeadersRewrite().Remove {
+						if resp.Header.Get(value) != "" {
+							resp.Header.Del(value)
+						}
+					}
+				}
+			}
+			return resp, nil
 		})
 	}, nil
 }
