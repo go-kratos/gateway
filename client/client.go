1a8f7ff54330fee4ee4f3cf812cfb0eb688970ec
realityone <realityone@me.com>
2022-10-08 12:04:41 +0800

    selector done with trailer

--- client/client.go
+++ client/client.go
@@ -42,11 +42,12 @@ func (c *client) RoundTrip(req *http.Request) (resp *http.Response, err error) {
 	startAt := time.Now()
 	resp, err = n.(*node).client.Do(req)
 	reqOpt.UpstreamResponseTime = append(reqOpt.UpstreamResponseTime, time.Since(startAt).Seconds())
-	done(ctx, selector.DoneInfo{Err: err})
 	if err != nil {
+		done(ctx, selector.DoneInfo{Err: err})
 		reqOpt.UpstreamStatusCode = append(reqOpt.UpstreamStatusCode, 0)
 		return nil, err
 	}
 	reqOpt.UpstreamStatusCode = append(reqOpt.UpstreamStatusCode, resp.StatusCode)
+	done(ctx, selector.DoneInfo{ReplyMeta: resp.Trailer})
 	return resp, nil
 }
