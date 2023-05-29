b228d7385189cb35c7721e90b5d0cc1ab1608cc0
Cidos <ginnerpeace@gmail.com>
2023-05-29 23:21:01 +0800

    test: add test for v1.Cors.AllowAllHeaders option

--- middleware/cors/cors_test.go
+++ middleware/cors/cors_test.go
@@ -21,12 +21,24 @@ func buildConfig(origins []string) *config.Middleware {
 	return &config.Middleware{Options: v}
 }
 
+func buildAllowAllHeaders() *config.Middleware {
+	v, err := anypb.New(&v1.Cors{
+		AllowOrigins:    []string{"*"},
+		AllowAllHeaders: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return &config.Middleware{Options: v}
+}
+
 func TestCors(t *testing.T) {
 	tests := []struct {
-		Config     *config.Middleware
-		Origin     string
-		Method     string
-		StatusCode int
+		Config          *config.Middleware
+		Origin          string
+		Method          string
+		StatusCode      int
+		AllowAllHeaders string
 	}{
 		{
 			Config:     &config.Middleware{},
@@ -93,6 +105,20 @@ func TestCors(t *testing.T) {
 			StatusCode: 200,
 		},
 		{
+			Config:          buildAllowAllHeaders(),
+			Origin:          "http://google.com",
+			Method:          "OPTIONS",
+			StatusCode:      200,
+			AllowAllHeaders: "Authorization,Content-Type,Source",
+		},
+		{
+			Config:          buildAllowAllHeaders(),
+			Origin:          "http://google.com",
+			Method:          "OPTIONS",
+			StatusCode:      200,
+			AllowAllHeaders: "X-PINGOTHER, Content-Type",
+		},
+		{
 			Config:     buildConfig([]string{"google.com"}),
 			Origin:     "http://google.com",
 			Method:     "GET",
@@ -119,6 +145,9 @@ func TestCors(t *testing.T) {
 				t.Fatal(err)
 			}
 			req.Header.Set(corsOriginHeader, test.Origin)
+			if test.AllowAllHeaders != "" {
+				req.Header.Set(corsRequestHeadersHeader, test.AllowAllHeaders)
+			}
 			resp, err := do.RoundTrip(req)
 			if err != nil {
 				t.Fatal(err)
@@ -143,8 +172,14 @@ func TestCors(t *testing.T) {
 				if v := resp.Header.Get(corsAllowMethodsHeader); v != strings.Join(defaultCorsMethods, ",") {
 					t.Fatalf("%d want %s but got %s", no, defaultCorsMethods, v)
 				}
-				if v := resp.Header.Get(corsAllowHeadersHeader); v != strings.Join(defaultCorsHeaders, ",") {
-					t.Fatalf("%d want %s but got %s", no, defaultCorsHeaders, v)
+				if test.AllowAllHeaders != "" {
+					if v := resp.Header.Get(corsAllowHeadersHeader); v != test.AllowAllHeaders {
+						t.Fatalf("%d want %s but got %s", no, test.AllowAllHeaders, v)
+					}
+				} else {
+					if v := resp.Header.Get(corsAllowHeadersHeader); v != strings.Join(defaultCorsHeaders, ",") {
+						t.Fatalf("%d want %s but got %s", no, defaultCorsHeaders, v)
+					}
 				}
 				if v := resp.Header.Get(corsMaxAgeHeader); v != "600" {
 					t.Fatalf("%d want %s but got %s", no, "600", v)
